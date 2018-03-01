c95593c54c30281bf83e147eaa084ee1990e50c7
pavoni <mail@gregdowling.com>
2018-03-01 10:28:06 +0000

    Bump socket.io to 0.5.7.2 - should fix connectivity problem (esp in hassio).

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 setup(name='pyloopenergy',
-      version='0.0.17',
+      version='0.0.18',
       description='Access Loop Energy energy monitors via Socket.IO API',
       url='http://github.com/pavoni/pyloopenergy',
       author='Greg Dowling',
       author_email='mail@gregdowling.com',
       license='MIT',
-      install_requires=['socketIO-client==0.5.6'],
+      install_requires=['socketIO-client==0.5.7.2'],
       packages=find_packages(),
       zip_safe=True)
