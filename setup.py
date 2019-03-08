74714e76152a53af3861f2ff939f5237e21d61d3
pavoni <mail@gregdowling.com>
2019-03-08 11:25:48 +0000

    Bump major version with socket.io update.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyloopenergy',
-      version='0.0.18',
+      version='0.1.0',
       description='Access Loop Energy energy monitors via Socket.IO API',
       url='http://github.com/pavoni/pyloopenergy',
       author='Greg Dowling',
